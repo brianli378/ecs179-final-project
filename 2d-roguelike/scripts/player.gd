2183aea8fea5368a2f8baf3eae161ffad5672d39
yAlex78 <85961347+yAlex78@users.noreply.github.com>
2025-11-11 20:56:20 -0800

    Added generic Gun class and Projectile class

--- 2d-roguelike/scripts/player.gd
+++ 2d-roguelike/scripts/player.gd
@@ -1,12 +1,13 @@
+class_name Player
 extends CharacterBody2D
 
 var speed = 700.0
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
 	
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var move_direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	
 	if move_direction != Vector2.ZERO:
